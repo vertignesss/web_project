182a1f7c1318a333e737cff78c6acc690fa937ac
vertignesss <proventus111@mail.ru>
2024-12-05 12:27:17 +0300

    display+import_export

--- lab/card_browse/admin.py
+++ lab/card_browse/admin.py
@@ -2,9 +2,47 @@ from django.contrib import admin
 
 from .models import Card, User, UserCard, Offer, Keyword
 from simple_history.admin import SimpleHistoryAdmin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+from import_export.fields import Field
 
-admin.site.register(Card)
+class CardResource(resources.ModelResource):
+
+	class Meta:
+		model = Card
+	def get_export_queryset(self, request=None):
+		return super().get_export_queryset(request)
+
+class CardAdmin(ImportExportModelAdmin):
+	resource_class = CardResource
+	list_display = ('name', 'oracle_text', 'power', 'toughness')
+	search_fields = ('name', 'oracle_text', 'power', 'toughness')
+
+class OfferResource(resources.ModelResource):
+	def dehydrate_user_card(self, offer):
+		name = getattr(offer.user_card.user, "login", "unknown")
+		card = getattr(offer.user_card.card, "name", "unknown")
+		return '%s has %s(s)' % (name, card)
+	class Meta:
+		model = Offer
+
+
+class OfferAdmin(ImportExportModelAdmin, SimpleHistoryAdmin):
+	resource_class = OfferResource
+	list_display = ('user_card', 'quantity', 'price')
+
+class UserCardResource(resources.ModelResource):
+	class Meta:
+		model = UserCard
+
+	def get_user(self, usercard):
+		return f"Пользователь: {user_card.user.login}"
+
+class UserCardAdmin(ImportExportModelAdmin, SimpleHistoryAdmin):
+	resource_class = UserCardResource
+	list_display = ('user', 'card')
 admin.site.register(User)
-admin.site.register(UserCard)
-admin.site.register(Offer, SimpleHistoryAdmin)
+admin.site.register(UserCard, UserCardAdmin)
+admin.site.register(Offer, OfferAdmin)
 admin.site.register(Keyword)
+admin.site.register(Card, CardAdmin)
--- lab/lab/settings.py
+++ lab/lab/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'card_browse.apps.BrowserConfig',
-    'simple_history'
+    'simple_history',
+    'import_export',
 ]
 
 MIDDLEWARE = [
